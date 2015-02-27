71dd38ab2b5f297ba9b0d6ce18aa8a27d52f7f35
Harrison <hs750@york.ac.uk>
2015-02-27 16:06:18 +0000

    initial implementation of home page. Queries for newest and most popular. Currently only output as tables -> needs styling

--- web2py/applications/Vesuvius/controllers/default.py
+++ web2py/applications/Vesuvius/controllers/default.py
@@ -8,17 +8,49 @@
 ## - download is for downloading files uploaded in the db (does streaming)
 ## - api is an example of Hypermedia API support and access control
 #########################################################################
+from operator import itemgetter
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+    # The newest 5 projects
+    newest = db((db.projects.id > 0) &
+                (db.projects.state == projectStates[0])).select(orderby=~db.projects.id, limitby=(0, 5))
+
+    # The most popular projects
+    # 'Popular' means projests which have the highest ratio of documents completed.
+    allDocs = db(db.documents.id > 0 ).select()
+    projectDocs = dict()
+    projectCompletedDocs = dict()
+
+    for doc in allDocs:
+        projID = doc.projectID
+        docsForProj = projectDocs.get(projID, 0)
+        projectDocs[projID] = docsForProj + 1
+        if doc.state == documentStates[1]:
+            compDocForProj = projectCompletedDocs.get(projID, 0)
+            projectCompletedDocs[projID] = compDocForProj + 1
+
+    projPercentateComplete = dict()
+
+    for projID in projectDocs.keys():
+        projCompleted = float(projectCompletedDocs.get(projID, 0)) / float(projectDocs.get(projID))
+        projPercentateComplete[projID] = projCompleted
+
+    projPercentateComplete = projPercentateComplete.items()
+
+    # Projects ordered by popularity, highest first
+    projPercentateComplete = sorted(projPercentateComplete, key=itemgetter(1), reverse=True)
+
+    # Top 5 must popular projects
+    projPercentateComplete = projPercentateComplete[0:5]
+
+    topFiveIDs = []
+    for i in range(0,min(5, len(projPercentateComplete))):
+        topFiveIDs.append(projPercentateComplete[i][0])
+
+    topFive = db(db.projects.id.belongs(topFiveIDs)).select()
+
+    return dict(new=newest, topFive=topFive)
 
 
 def user():
