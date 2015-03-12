5341ce59763839bf81e7a241cc7020ddea0f246c
Harrison <hs750@york.ac.uk>
2015-03-12 19:03:20 +0000

    show documents as being 'done' rather than hiding them

--- applications/Vesuvius/controllers/project.py
+++ applications/Vesuvius/controllers/project.py
@@ -2,7 +2,7 @@
 def view():
     projID = request.vars.get('id')
     project = db(db.projects.id == projID).select()[0]
-    projectDocs = db((db.documents.projectID == projID) & (db.documents.state == documentStates[0])).select()
+    projectDocs = db(db.documents.projectID == projID).select()
     owner = db(db.auth_user.id == project.userID).select(db.auth_user.username)[0]
     return dict(project=project, docs=projectDocs, owner=owner)
 
